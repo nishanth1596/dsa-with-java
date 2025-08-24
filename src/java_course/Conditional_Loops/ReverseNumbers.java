088f9db65f8817afa6729e9893f95ae2c885fda5
Nishanth <nishanth1596@gmail.com>
2025-08-24 17:42:12 +0530

    refactor the logic

--- src/java_course/Conditional_Loops/ReverseNumbers.java
+++ src/java_course/Conditional_Loops/ReverseNumbers.java
@@ -4,10 +4,10 @@ public class ReverseNumbers {
     public static void main(String[] args) {
         int n = 123456789;
 
-        String reversedNumber = "";
+        int reversedNumber = 0;
         while (n > 0){
             int lastNumber = n % 10;
-            reversedNumber += lastNumber;
+            reversedNumber = reversedNumber * 10  + lastNumber;
             n /= 10;
         }
         System.out.println(reversedNumber);
